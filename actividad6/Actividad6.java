33a157213f124b8823b0bf4bfcd89bb32a214636
mrPenisParker <juanjosecanast@gmail.com>
2023-11-24 19:47:20 -0500

    Actividad 6

---
+++ actividad6/Actividad6.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad6;
+public class Actividad6 {
+    public static void main(String[] args) {
+        Interfaz Ventana1 = new Interfaz ();
+        Ventana1.setVisible(true);
+    } 
+}
